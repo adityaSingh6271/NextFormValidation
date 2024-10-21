3f19a9c08f9592c0a2d1d89bae8ab2c18f4e2fb8
Aditya Singh <singhaditya6271@gmail.com>
2024-10-21 13:00:38 +0530

    final changes made

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  trailingSlash: true, // Add this line for static exports
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
